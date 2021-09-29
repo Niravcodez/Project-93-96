09ed4e733d2d294cc44c304cd64c4715b7cbda1b
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2021-09-28 17:18:54 -0700

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,6 +10,9 @@ var firebaseConfig = {
 
     firebase.initializeApp(firebaseConfig);
 
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
 function send()
 {
       msg = document.getElementById("msg").value;
@@ -21,12 +24,33 @@ function send()
       document.getElementById("msg").innerHTML = "";
 }
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
-         firebase_message_id = childKey;
-         message_data = childData;
+      firebase_message_id = childKey;
+      message_data = childData;
 
+      console.log(firebase_message_id);
+      console.log(message_data);
+      name = message_data["name"];
+      message = message_data["message"];
+      like = message_data["like"];
+      name_with_tag = "<h4>" + name + "<img src = 'tick.png' class = 'user_tick'></h4>";
+      message_with_tag = "<h4 class = 'message_h4'>" + message + "</h4>";
+      like_with_tag = "<button class = 'btn btn-warning' id = " + firebase_message_id + "value = " + like + "onclick = 'updateLike(this.id)'>";
+      span_with_tag = "<span class = 'glyphicon glyphicon-thumbs-up' > like: " + like + "</span></button><hr>";
+      row = name_with_tag + message_with_tag + like_with_tag + span_with_tag;
+      document.getElementById("output").innerHTML += row;
 
       } });  }); }
 getData();
+
+function updateLike(message_id)
+{
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
+      updated_likes = Number(likes) + 1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like: updated_likes
+      });
+}
 
 function logOut()
 {
@@ -35,3 +59,9 @@ function logOut()
 
       window.location.replace("index.html");
 }
+
+function back()
+{
+      window.location.replace("kwitter_room.html")
+}
+
